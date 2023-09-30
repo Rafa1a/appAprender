0edc4d4163f8efe3b018754b312e40968f1ffb5c
Rafael Leal Altero <rafa.altero@gmail.com>
2023-09-30 18:03:19 -0300

    up

---
+++ Ambiente1/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text } from "react-native";
+
+
+function App () {
+    const tsx =  <Text>Primerio componente </Text>
+    return tsx;
+}
+
+export default App;
